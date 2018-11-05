4306dcce5447f28b8fa87badf19405ba47e435e8
Egor00101 <43553542+Egor00101@users.noreply.github.com>
2018-11-05 23:37:33 +0300

    Update Skype.cpp

--- Skype.cpp
+++ Skype.cpp
@@ -42,12 +42,29 @@
     }
     cout << a << " " << b <<"\n";
 }
+int f4(int a, int b, int c)
+{
+ if(a>b&&a>c)
+ {
+  return a ; 
+ }
+  if(b>a&&b>c)
+  {
+   return b ; 
+  }
+  if(c>a&&c>b)
+  {
+   return c; 
+  }
+}
   int main()
 {
   int a;
   int b;
+  int c;
   cout << f1(4)<<"\n";
     f2(3,5);
     f3(5,7);
+   cout<<f4(1,3,5)<<"\n";
   return 0; 
 }
